29f37f31cc58aadad817936908eb09f645cd75cb
Alberto Sottile <alby128@gmail.com>
2019-05-05 16:03:21 +0200

    Travis: restructure bottle creation and upload

--- bottle_mover.py
+++ bottle_mover.py
@@ -13,6 +13,6 @@ for formula in data.keys():
     tags = data[formula]['bottle']['tags']
     for osName in tags.keys():
         localName = tags[osName]['local_filename']
-        remoteName = "../" + tags[osName]['filename']
+        remoteName = tags[osName]['filename']
         print(("Bottling {} for {}: renaming {} to {}").format(formula, osName, localName, remoteName))
         os.rename(localName, remoteName)
